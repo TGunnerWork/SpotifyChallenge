b677a0f693070fd37860a9017df9d8aef4ed9813
Gunner <tgwork11@gmail.com>
2023-12-27 12:02:26 -0600

    Fine-tuned Collaborative Filter creation

--- CreateCollFilter.py
+++ CreateCollFilter.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import sqlite3
+from scipy.sparse import csr_matrix, save_npz
 from sklearn.metrics.pairwise import cosine_similarity
-import scipy.sparse as spsparse
 
 # gather information
 with sqlite3.connect('Spotify.db') as conn:
     df = pd.read_sql_query(
         "SELECT DISTINCT playlist_id, track_id FROM PlaylistTracks;",
-        conn)
+        conn
+    )
 
 # create sparse matrix
-csr = spsparse.csr_matrix(([1]*len(df), (df['playlist_id'], df['track_id'])))
+csr = csr_matrix(([1]*len(df), (df['playlist_id'], df['track_id'])))
 
 # generate similarity scores
 sim = cosine_similarity(csr.T, dense_output=False)
 
 # save model
-# spsparse.save_npz("col_fil_spotify.npz", sim)
+save_npz("col_fil_spotify.npz", sim)
