4ed0bcda1bd6ba18dcdba6f38219e2c8b13d26a1
Gunner <tgwork11@gmail.com>
2023-12-29 23:27:45 -0600

    Reworked InitializeMMPL.py

--- InitializeMMPL.py
+++ InitializeMMPL.py
@@ -2,24 +2,9 @@ import json
 import pandas as pd
 import os
 import sqlite3
-# from send_email import send_email
-# import time
-# from datetime import timedelta
-
-
-# Iterate over all JSON files and load data to RawData table
-def process_json(file):
-    with sqlite3.connect("Spotify.db") as connection:
-        with open(file, "r") as proc_file:
-            pd.json_normalize(
-                json.load(proc_file)['playlists'],
-                record_path='tracks', meta=meta, meta_prefix='pl_', errors='ignore'
-            )[columns].to_sql("RawData", connection, if_exists='append', index=False)
-
 
 # Set directories/file paths
-wd = os.getcwd()
-source_data = os.path.join(os.getcwd(), r'SourceData\spotify_million_playlist_dataset\data')
+source_data = "SourceData/spotify_million_playlist_dataset/data"
 
 columns = [
     'pl_pid',
@@ -70,30 +55,19 @@ dtypes = {
     'album_uri': str,
     'artist_uri': str
 }
-progress = 1
-
-# start = time.time()
 
 # Create Tables
-with sqlite3.connect(os.path.join(wd, "Spotify.db")) as conn:
+with sqlite3.connect("Spotify.db") as conn:
     cursor = conn.cursor()
-    with open(os.path.join(wd, "Queries", "CreateTables.sql"), 'r') as script_file:
+    with open("Queries/CreateTables.sql", 'r') as script_file:
         cursor.executescript(script_file.read())
         conn.commit()
 
-# send_email("CreateTables.sql has completed.",
-#           f'Process completed at {time.strftime("%H:%M:%S", time.localtime(time.time()))}.' +
-#           f'\nProcess took {str(timedelta(seconds=time.time()-start))}')
+    # Import data into DB
+    for json_file in os.listdir(source_data):
+        with open(os.path.join(source_data, json_file), 'r') as file:
+            pd.json_normalize(
+                json.load(file)['playlists'],
+                record_path='tracks', meta=meta, meta_prefix='pl_', errors='ignore'
+            )[columns].to_sql("RawData", conn, if_exists='append', index=False)
 
-# start = time.time()
-
-# Import data into DB
-for json_file in os.listdir(os.path.join(wd, source_data)):
-    print(f"Processing JSON {progress} of 1,000.")
-    process_json(os.path.join(os.path.join(wd, source_data), json_file))
-    progress += 1
-
-# send_email("JSON Import has completed.",
-#           'All JSON files have been imported into database.\n' +
-#           f'Process completed at {time.strftime("%H:%M:%S", time.localtime(time.time()))}.' +
-#           f'\nProcess took {str(timedelta(seconds=time.time()-start))}')
